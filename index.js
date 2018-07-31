501404fc46b69b34dc7681521cc84fc222b6b6fc
cqc <285497458@qq.com>
2018-07-31 17:25:57 +0800

    html2canvas demo

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var server = require('http').createServer(app);
 app.use('/', express.static(__dirname + '/static')); 
 app.use('/lib', express.static(__dirname + '/lib'));
 app.use('/js', express.static(__dirname + '/js'));
+app.use('/imgs', express.static(__dirname + '/imgs'));
+app.use('/bc', express.static(__dirname + '/bower_components'));
 var port = 8080;
 console.log("launch nodejs server at port: " + port);
 //listening port
