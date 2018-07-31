a10a684ebd048a43d9291a7632e4467b73c16f36
cqc <285497458@qq.com>
2018-07-31 12:45:24 +0800

    introduce grunt

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		//读取package.json文件
+		pkg: grunt.file.readJSON('package.json'),
+	    // Empties folders to start fresh
+	    clean: {
+	    	another:'app/dist/ng*.js',
+			dist: 'app/dist/*.js'
+	    },
+		//concat用来合并js文件
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['js/deferAsync/*.js'],
+				dest: 'app/dist/<%= pkg.name %>.js'
+			}
+		},
+		//uglify用来压缩js文件
+		uglify: {
+			options: {
+			// 此处定义的banner注释将插入到输出文件的顶部
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+				//uglify会自动压缩concat任务中生成的文件
+					'app/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+				}
+			}
+		}
+
+	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.registerTask("myClean", ['clean:dist']);
+	grunt.registerTask('test', ['myClean', 'concat', 'uglify']);
+	grunt.registerTask('testNew', ['clean:another', 'concat', 'uglify']);
+	grunt.registerTask('default', ['test']);
+};
